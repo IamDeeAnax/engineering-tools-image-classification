bb307277d9cc27375f78fe2f2e6708d93128bf39
Emmanuel Olowoboko <111533591+IamDeeAnax@users.noreply.github.com>
2024-06-19 21:32:20 +0100

    Update app.py
    Add background image
    Add custom css

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -4,6 +4,7 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
 import tensorflow_hub as hub
+import base64
 
 # Define a function to load the model with custom objects
 def load_model_with_custom_objects(model_path):
@@ -12,18 +13,56 @@ def load_model_with_custom_objects(model_path):
 
 # Load the best model
 try:
-    model = load_model_with_custom_objects('Streamlit/model/engineering_tools_image_classification_model.keras')
-    st.title('Engineering Tools Image Classification')
+    model = load_model_with_custom_objects('model/engineering_tools_image_classification_model.keras')
+    background_image_path = 'assets/background.png'
+    
+    def add_bg_from_local(image_file):
+        with open(image_file, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+            background-size: cover;
+        }}
+        .stApp h1 {{
+            color: black;  /* Title color */
+        }}
+        .stApp .stMarkdown {{
+            color: black;  /* Markdown text color */
+        }}
+        .stApp .stButton>button {{
+            color: black;  /* Button text color */
+        }}
+        .stApp .stError {{
+            color: red;  /* Error message color */
+        }}
+
+        </style>
+        """,
+        unsafe_allow_html=True
+        )
+    add_bg_from_local(background_image_path)
+    
+    st.markdown("<h1 class='heading'>Engineering Tools Image Classification</h1>", unsafe_allow_html=True)
 
     # Determine the input shape of the model
     input_shape = model.input_shape[1:3]  # e.g., (224, 224) or (299, 299)
 
+    # Initialize session state for the uploaded image
+    if 'uploaded_file' not in st.session_state:
+        st.session_state.uploaded_file = None
+
     # Upload image
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
+        st.session_state.uploaded_file = uploaded_file
+
+    if st.session_state.uploaded_file is not None:
         try:
-            img = Image.open(uploaded_file)
+            img = Image.open(st.session_state.uploaded_file)
             st.image(img, caption='Uploaded Image.', use_column_width=True)
 
             # Ensure image has three channels (RGB)
@@ -47,5 +86,8 @@ try:
             st.write(f"The image is classified as: {class_names[predicted_class[0]]}")
         except Exception as e:
             st.error(f"An error occurred during prediction: {e}")
+    else:
+        st.write("Please upload an image to get started.")
 except Exception as e:
     st.error(f"An error occurred while loading the model: {e}")
+
